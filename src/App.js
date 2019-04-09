ba70c755865ff99bb4904fc45bef51e428b8c6f9
Kamil Drazkiewicz <k.drazkiewicz@getprintbox.com>
2019-04-09 08:44:44 +0200

    Add Redux and redux dev tools

--- src/App.js
+++ src/App.js
@@ -3,6 +3,32 @@ import './App.scss';
 import DashBoard from "./components/DashBoard/DashBoard";
 import NavBar from "./components/NavBar/NavBar";
 
+import { composeWithDevTools} from "redux-devtools-extension";
+import { createStore } from "redux"
+
+const initialTasks = {
+	project:['Dev','Msw'],
+	tasks: [
+		'Work', 'House', 'Job'
+	]
+};
+
+function tasks(state = initialTasks , action) {
+	switch (action.type){
+		case 'ADD':
+			return{...state, tasks:[...state.tasks, action.movie]}
+			break;
+		case 'RESET':
+			return {...state,tasks:[]};
+		default:
+			return state
+	}
+}
+
+const store = createStore(tasks, composeWithDevTools());
+
+window.store = store;
+
 class App extends Component {
   render() {
     return (
