9852f86a685405599c0a8e913205f12b789bb662
Kamil Drazkiewicz <k.drazkiewicz@getprintbox.com>
2019-04-16 10:08:00 +0200

    add function in Tasks.js

--- src/components/DashBoard/MyTasks/Tasks/Tasks.js
+++ src/components/DashBoard/MyTasks/Tasks/Tasks.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const Tasks = ({tasks}) =>
+const Tasks = ({tasks}) =>(
   <ul>
     {tasks.list.map(tasks => <li>{tasks}</li>)}
-  </ul>
+  </ul>);
 
 const mapStateToProps = (state) => ({
   tasks: state.tasks
 })
 
-export default connect(
-  mapStateToProps,
-)(Tasks)
+export default connect(mapStateToProps,null)(Tasks)
